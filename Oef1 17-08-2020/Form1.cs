4fee426053026d4d5066aa6d0f2296f6d5638c29
Sergio Corrente <corrente.sergio@outlook.be>
2020-08-17 15:52:33 +0200

    Not completed

---
+++ Oef1 17-08-2020/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Oef1_17_08_2020
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       List<Person> personsList = new List<Person>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            personsList.Add(new Person("Johan", 23));
+            personsList.Add(new Person("Koen", 44));
+            personsList.Add(new Person("Ken"));
+
+            listBox1.DataSource = personsList;
+
+            personsList.Add(new Person("Jonas", 24));
+
+           listBox1.DataSource = null;
+           listBox1.DataSource = personsList;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.DataSource != null)
+            {
+                if (personsList[listBox1.SelectedIndex].Leeftijd >= 0) {
+                    textBox1.Text = personsList[listBox1.SelectedIndex].Leeftijd.ToString();
+                }
+                else { 
+                    textBox1.Text = "Niet bekend";
+                }
+            }
+        }
+
+    }
+}
---
+++ Oef1 17-08-2020/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oef1_17_08_2020
+{
+    public class Person
+    {
+
+        public string Naam { get; set; }
+        public int Leeftijd { get; set; }
+
+        public Person(string naam, int leeftijd)
+        {
+            Naam = naam;
+            Leeftijd = leeftijd;
+
+        }
+        public Person(string naam)
+        {
+            Naam = naam;
+            Leeftijd = -1;
+        }
+        public override string ToString()
+        {
+            return Naam;
+        }
+    }
+}
